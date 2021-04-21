1a8d2f736ec22ecf0c36a177a5831af370b93e6f
Likael <glasekkamil@gmail.com>
2021-04-21 18:48:10 +0200

    Add send and reset buttons

--- frontend/src/components/Draw.js
+++ frontend/src/components/Draw.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {SketchField, Tools} from 'react-sketch2';
 import {Button} from 'react-bootstrap'
 
+
 const Draw = () => {
+    const sketch = useRef()
 
+    const handleReset = () => {
+        const canvas = sketch.current.clear();
+        sketch.current._backgroundColor('black')
+    }
+    const sendData = () => {
+        const canvas = sketch.current.toDataURL()
+        console.log(canvas)
+    }
 
+    const getResult = (id) => {
+
+    }
     return (
         <React.Fragment>
-            <SketchField width='500px'
-                         height='500px'
-                         tool={Tools.Pencil}
-                         lineColor='white'
-                         style={{margin : '0 auto'}}
-                         backgroundColor='black'
-                         imageFormat='jpg'
-                         lineWidth={30}/>
+            <SketchField
+                ref={sketch}
+                width='500px'
+                height='500px'
+                tool={Tools.Pencil}
+                lineColor='white'
+                style={{margin: '0 auto'}}
+                backgroundColor='black'
+                imageFormat='jpg'
+                lineWidth={30}/>
             <div className='mt-3'>
-                <Button onClick={()=>{}} variant="primary">Send</Button>
-                <Button onClick={()=>{}} variant="danger">Reset</Button>
+                <Button onClick={sendData} variant="primary">Send</Button>
+                <Button onClick={handleReset} variant="danger">Reset</Button>
             </div>
         </React.Fragment>
     )
