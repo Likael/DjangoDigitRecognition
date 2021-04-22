4bc695d5536b092ab3ae2aed7bd84d2161fb8544
Likael <glasekkamil@gmail.com>
2021-04-22 22:00:58 +0200

    Implement model`s save method Overriden save method now loads and preprocesses image and predicts number using keras model saved as a h5 file

--- backend/digit/models.py
+++ backend/digit/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.conf import settings
+from PIL import Image
+from keras.preprocessing import image
+from keras.models import load_model
+import tensorflow as tf
+import cv2
+import numpy as np
+import os
 
 
 # Create your models here.
@@ -12,5 +20,29 @@ class Digit(models.Model):
     def __str__(self):
         return str(self.id)
 
-    # def save(self, *args, **kwargs):
-    #     return super().save(self, *args, *kwargs)
+    def save(self, *args, **kwargs):
+        print(self.image)
+        img = Image.open(self.image)
+        img = image.img_to_array(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
+        print(img.shape)
+        img = np.expand_dims(img, axis=2)
+        img = np.expand_dims(img, axis=0)
+        model_name = 'digit_recognition_model.h5'
+        try:
+            path = os.path.join(settings.BASE_DIR, model_name)
+            model = load_model(path)
+            pred = model.predict(img)
+            self.result = f'Number is recognized as {np.argmax(pred)}'
+        except Exception as e:
+            print(e.__traceback__)
+            self.result = 'Failed to classify'
+
+        return super().save(*args, **kwargs)
+
+
+def categorize_back(arr):
+    for a, x in enumerate(np.nditer(arr)):
+        if x == 1.:
+            return a
